bfe90bba4a3098b62c2877e87623450191e5beb0
longbiu <longbiu@foxmail.com>
2022-04-30 22:58:22 +0800

    fix: use copy_util

--- src/main/java/com/biu/wiki/service/EbookService.java
+++ src/main/java/com/biu/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.biu.wiki.domain.EbookExample;
 import com.biu.wiki.mapper.EbookMapper;
 import com.biu.wiki.req.EbookReq;
 import com.biu.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.biu.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,6 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
