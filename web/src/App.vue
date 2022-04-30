db556fd9203d9d3659e5b17d19bca37975b6a1e3
longbiu <longbiu@foxmail.com>
2022-05-01 02:02:39 +0800

    feat: use router-view and fix es lint's rule

--- web/src/App.vue
+++ web/src/App.vue
@@ -14,56 +14,7 @@
       </a-menu>
     </a-layout-header>
     <a-layout-content style="padding: 0 50px">
-      <a-layout style="padding: 24px 0; background: #fff">
-        <a-layout-sider width="200" style="background: #fff">
-          <a-menu
-              v-model:selectedKeys="selectedKeys2"
-              v-model:openKeys="openKeys"
-              mode="inline"
-              style="height: 100%"
-          >
-            <a-sub-menu key="sub1">
-              <template #title>
-                <span>
-                  <user-outlined/>
-                  subnav 1
-                </span>
-              </template>
-              <a-menu-item key="1">option1</a-menu-item>
-              <a-menu-item key="2">option2</a-menu-item>
-              <a-menu-item key="3">option3</a-menu-item>
-              <a-menu-item key="4">option4</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-              <template #title>
-                <span>
-                  <laptop-outlined/>
-                  subnav 2
-                </span>
-              </template>
-              <a-menu-item key="5">option5</a-menu-item>
-              <a-menu-item key="6">option6</a-menu-item>
-              <a-menu-item key="7">option7</a-menu-item>
-              <a-menu-item key="8">option8</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-              <template #title>
-                <span>
-                  <notification-outlined/>
-                  subnav 3
-                </span>
-              </template>
-              <a-menu-item key="9">option9</a-menu-item>
-              <a-menu-item key="10">option10</a-menu-item>
-              <a-menu-item key="11">option11</a-menu-item>
-              <a-menu-item key="12">option12</a-menu-item>
-            </a-sub-menu>
-          </a-menu>
-        </a-layout-sider>
-        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-          Content
-        </a-layout-content>
-      </a-layout>
+      <router-view/>
     </a-layout-content>
     <a-layout-footer style="text-align: center">
       biu_wiki ©2022 Created by longbiu
