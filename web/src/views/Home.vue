0a5c8a148c2b80b47d6c716afd658f1f7c32d942
longbiu <longbiu@foxmail.com>
2022-05-01 00:59:12 +0800

    feat: import ant_design_vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <p>what happen?</p>
+    <a-button type="dashed">
+      Dashed
+    </a-button>
+    <a-button type="danger">
+      Danger
+    </a-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
