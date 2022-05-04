550e095a8eb8b7a37fa0b8b699c413f9b5f9aaeb
longbiu <longbiu@foxmail.com>
2022-05-04 20:41:23 +0800

    feat: import ant_design icons and use it

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,16 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import Antd from 'ant-design-vue';
 import App from './App.vue';
 import 'ant-design-vue/dist/antd.css';
 import router from './router';
 import store from './store';
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+const icons: any = Icons;
+
+for (const i in icons) {
+    app.component(i, icons[i])
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -44,10 +44,27 @@
       </a-menu>
     </a-layout-sider>
     <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-      <pre>
-        {{ ebooks }}
-        {{ ebooks2 }}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="ebooks">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar>
+                <a-avatar :src="item.avatar"/>
+              </template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
@@ -56,6 +73,20 @@
 import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from "axios";
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
@@ -81,7 +112,19 @@ export default defineComponent({
     });
     return {
       ebooks,
-      ebooks2: toRef(ebooks1, "books")
+      ebooks2: toRef(ebooks1, "books"),
+      listData,
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        {type: 'StarOutlined', text: "156"},
+        {type: 'LikeOutlined', text: '156'},
+        {type: 'MessageOutlined', text: '2'}
+      ],
     }
   }
 });
