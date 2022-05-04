35cf0838af3291a1ea388d41fab4e433ddc9edc0
longbiu <longbiu@foxmail.com>
2022-05-04 22:11:47 +0800

    fix: fix baseurl for axios

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,9 @@ import 'ant-design-vue/dist/antd.css';
 import router from './router';
 import store from './store';
 import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -102,7 +102,7 @@ export default defineComponent({
     onMounted(() => {
       console.log("onMounted")
       // eslint-disable-next-line no-undef
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then(function (response) {
+      axios.get("/ebook/list").then(function (response) {
         // eslint-disable-next-line no-undef
         const data = response.data
         ebooks.value = data.content
