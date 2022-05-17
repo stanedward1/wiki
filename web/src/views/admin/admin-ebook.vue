82af84cfc65a43c95846c597de968d657a0356b9
longbiu <longbiu@foxmail.com>
2022-05-18 00:45:33 +0800

    feat: update layout about ebook manage

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>电子书管理</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ padding: '15px 24px', minHeight: '280px' }">
+      <div class="about">
+        <h1>电子书管理</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
