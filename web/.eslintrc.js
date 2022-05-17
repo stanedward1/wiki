bedd34d852050fc3afcb09be4b7cede68b50b3c8
longbiu <longbiu@foxmail.com>
2022-05-18 00:49:57 +0800

    feat: update es6's rule

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'vue/no-unused-components': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
+        '@typescript-eslint/no-explicit-any': 'off',
+        'vue/no-unused-vars': 'off'
     }
 }
