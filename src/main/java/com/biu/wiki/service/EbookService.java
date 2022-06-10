cbd1e5f517315eb9c5f9b2cee4d67f3041cbd67b
longbiu <longbiu@foxmail.com>
2022-06-10 11:52:32 +0800

    feat: update dependency and add log

--- src/main/java/com/biu/wiki/service/EbookService.java
+++ src/main/java/com/biu/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.biu.wiki.mapper.EbookMapper;
 import com.biu.wiki.req.EbookReq;
 import com.biu.wiki.resp.EbookResp;
 import com.biu.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -18,16 +22,24 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    public static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
+        PageHelper.startPage(1, 3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总行数：{}", pageInfo.getPages());
+
         return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
