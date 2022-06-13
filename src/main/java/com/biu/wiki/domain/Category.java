cbb7e626e1d6ba9935d8436cf7ec608a7ff6fec2
longbiu <longbiu@foxmail.com>
2022-06-13 20:34:03 +0800

    update: push generated files

---
+++ src/main/java/com/biu/wiki/domain/Category.java
@@ -0,0 +1,53 @@
+package com.biu.wiki.domain;
+
+public class Category {
+    private Long id;
+
+    private Long parent;
+
+    private String name;
+
+    private Integer sort;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getParent() {
+        return parent;
+    }
+
+    public void setParent(Long parent) {
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", parent=" + parent +
+                ", name='" + name + '\'' +
+                ", sort=" + sort +
+                '}';
+    }
+}
