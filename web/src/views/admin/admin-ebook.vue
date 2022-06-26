695aafa44f077e589127f5b170f3d6efba169501
longbiu <longbiu@foxmail.com>
2022-06-26 23:29:30 +0800

    feat: ebook use cascade

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -57,11 +57,10 @@
       <a-form-item label="名称">
         <a-input v-model:value="ebook.name"/>
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1Id"/>
-      </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category2Id"/>
+      <a-form-item>
+        <a-cascader v-model:value="categoryIds"
+                    :field-names="{label:'name',value:'id',children:'children'}"
+                    :options="level1"/>
       </a-form-item>
       <a-form-item label="描述">
         <a-input v-model:value="ebook.description" type="text"/>
@@ -167,11 +166,14 @@ export default defineComponent({
     };
 
     // 表单
+    const categoryIds = ref();
     const ebook = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
 
       axios.post("/ebook/save", ebook.value).then((response) => {
             modalLoading.value = false;
@@ -195,7 +197,8 @@ export default defineComponent({
     // 编辑
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = Tool.copy(record)
+      ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
     };
 
     // 新增
@@ -216,7 +219,27 @@ export default defineComponent({
       });
     };
 
+    const level1 = ref();
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          const categorys = data.content;
+          console.log("原始数组: ", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构: ", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     onMounted(() => {
+      handleQueryCategory();
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
@@ -230,6 +253,8 @@ export default defineComponent({
       columns,
       loading,
       handleTableChange,
+      categoryIds,
+      level1,
 
       edit,
       add,
